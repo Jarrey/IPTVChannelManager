cd4bb71dabec0b556962f97795c996c4a0fdeb75
jarrey <jar.bob@gmail.com>
2026-04-17 22:40:18 +0800

    优化图片显示性能

--- IPTVChannelManager/Common/ImgConverter.cs
+++ IPTVChannelManager/Common/ImgConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
@@ -7,21 +8,33 @@ namespace IPTVChannelManager.Common
 {
     public class ImageConverter : IValueConverter
     {
+        private static readonly ConcurrentDictionary<string, BitmapImage> _cache = new();
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value != null)
+            var url = value?.ToString();
+            if (string.IsNullOrEmpty(url))
+                return null;
+
+            if (_cache.TryGetValue(url, out var cached))
+                return cached;
+
+            try
             {
                 BitmapImage bi = new BitmapImage();
                 bi.BeginInit();
-                bi.UriSource = new Uri(value?.ToString(), UriKind.Relative);
+                bi.UriSource = new Uri(url, UriKind.Relative);
+                bi.DecodePixelWidth = 80;
+                bi.CacheOption = BitmapCacheOption.Default;
                 bi.EndInit();
+
+                _cache.TryAdd(url, bi);
                 return bi;
             }
-            else
+            catch
             {
                 return null;
             }
-
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
